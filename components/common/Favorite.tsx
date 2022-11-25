454f897b62fc32c90943ca2136a65ebb35c45119
Daichi Natsume <taylor.1d.soccer@icloud.com>
2022-11-26 00:46:51 +0900

    お気に入りマークの微調整

--- components/common/Favorite.tsx
+++ components/common/Favorite.tsx
@@ -1,16 +1,31 @@
 //お気に入りアイコンの表示に関してまとめたコンポーネント
 
+import { useState } from "preact/hooks";
+
 interface Props {
   num?: number;
 }
 
 //丸枠で囲まれたお気に入りアイコン
 export function Favorite(props: Props) {
+  const [fav, setFav] = useState(true);
+  const FavFunction = () => {
+    if (fav) {
+      setFav(false);
+    } else {
+      setFav(true);
+    }
+  };
   return (
     <div class="w-10 h-10 rounded-full border p-2.5 border-brown">
       <img
-        src="/icon/common/four-icons/favorite-brown.png"
+        src={`${
+          fav
+            ? "/icon/common/four-icons/favorite-brown.png"
+            : "/icon/common/four-icons/after-favorite.png"
+        }`}
         alt="お気に入りアイコン"
+        onClick={FavFunction}
       />
     </div>
   );
@@ -18,15 +33,28 @@ export function Favorite(props: Props) {
 
 //丸枠で囲まれたお気に入りアイコンとお気に入りされた数を表示
 export function FavoriteNum(props: Props) {
+  const [fav, setFav] = useState(true);
+  const FavFunction = () => {
+    if (fav) {
+      setFav(false);
+    } else {
+      setFav(true);
+    }
+  };
   return (
     <div class="w-14 h-14 border rounded-full flex items-center absolute bottom-2 right-2 z-50 bg-white border-brown">
       <div class="flex flex-col text-center mx-auto">
         <img
-          src="/icon/common/four-icons/after-favorite.png"
+          src={`${
+            fav
+              ? "/icon/common/four-icons/favorite-brown.png"
+              : "/icon/common/four-icons/after-favorite.png"
+          }`}
           alt="お気に入り"
           class="w-6 h-6 my-1"
+          onClick={FavFunction}
         />
-        <p class="text-2.5">{props.num}</p>
+        <p class="text-2.5">{fav ? props.num! - 1 : props.num}</p>
       </div>
     </div>
   );
